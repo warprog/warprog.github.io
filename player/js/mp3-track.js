72facc6cb870a3fc85b9ce8fa5161cfa44655e23
fill <test_host@list.ru>
2018-05-01 21:30:12 +0300

    commit

---
+++ player/js/mp3-track.js
@@ -0,0 +1,36 @@
+/*! jQuery v1.12.4 | (c) jQuery Foundation | jquery.org/license */
+        $('.mplayer-1').mplayer({
+            'volume': 80,
+            'playlist': [
+            	{
+                    "title": "The Police",
+                    "author": "Eve<wbr>ry<wbr> Breath You<wbr> Ta<wbr>ke",
+                    "pfile": "https://drive.google.com/file/d/1sTNimloxTJ-yjpaD8jFCphO_qCjLNy_p/view",
+                },
+               
+            ]
+        });
+	
+        $('.mplayer-2').mplayer({
+            'volume': 80,
+            'playlist': [
+            	{
+                    "title": "Косынка",
+                    "author": "Военный шансон",
+                    "pfile": "https://cs9-3v4.vkuseraudio.net/p24/086c900a1e8021.mp3",
+                },
+               
+            ]
+        });
+	
+        $('.mplayer-3').mplayer({
+            'volume': 80,
+            'playlist': [
+            	{
+                    "title": "Опустилась ночь",
+                    "author": "военный шансон",
+                    "pfile": "https://psv4.vkuseraudio.net/c815324/u436994751/audios/3cbbf84bc83e.mp3?extra=_I_EK92c8JEw_NhulZprzQQtistTlh4y6V9ISuv9fOM4vFwwt3U_wCbnKZC_srWazHWZ_ALxNPVrmxgWsyzm9XS6oW9DgeHBTVDgol8-dlBjVXHkbH6P-jwyYHMp7syH25L5XiRwf1A",
+                },
+               
+            ]
+        });
